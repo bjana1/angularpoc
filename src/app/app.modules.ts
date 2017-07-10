3753d35b67608871060ea13135c354b2bc13b4b8
bjana1 <brindabatijana@yahoo.com>
2017-07-10 08:56:59 -0700

    add new file and do a commit for testing

---
+++ src/app/app.modules.ts
@@ -0,0 +1,13 @@
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
